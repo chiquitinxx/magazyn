317807d915563d84aeda51a5c7e6359884beba1d
Jorge Franco <jorge.franco.leza@gmail.com>
2024-06-05 19:46:58 +0200

    Initial module

---
+++ api/src/test/java/dev/yila/magazyn/ToRemoveTest.java
@@ -0,0 +1,13 @@
+package dev.yila.magazyn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToRemoveTest {
+
+    @Test
+    public void isTrue() {
+        assertTrue(1 == 1);
+    }
+}
